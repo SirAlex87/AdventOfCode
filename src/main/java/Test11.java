91a070094949a79eb7861a2458e79a411ab510ac
EUROBET-ITALIA\alessandro.borzacchi <alessandro.borzacchiello@eurobet.it>
2026-05-04 23:42:40 +0200

    parte 2 task 11

--- src/main/java/Test11.java
+++ src/main/java/Test11.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import service.ServiceTest10;
@@ -23,7 +24,51 @@ public class Test11 {
 
 		ServiceTest11 serviceTest11 = new ServiceTest11();
 		//extracted1(data, serviceTest11, "you");
-		extracted2(data, serviceTest11,"svr");
+		//extracted2(data, serviceTest11,"svr");
+		extractet2SecondVersion(data, serviceTest11,"svr");
+	}
+
+	private static void extractet2SecondVersion(String data, ServiceTest11 serviceTest11, String partenza) {
+		String[] strParts = data.split("\n");
+		Arrays.stream(strParts).forEach(s -> serviceTest11.populateList(s));
+		BigInteger resultfftout = serviceTest11.countPaths("fft", "out", serviceTest11.getMappaConnessioni(), new HashMap<>());
+		//BigInteger result = serviceTest11.countPaths("svr", "out", serviceTest11.getMappaConnessioni(), new HashMap<>(), "fft", "dac",0);
+        System.out.println("Numero totale di path fft out: " + resultfftout);
+        BigInteger resultdacout = serviceTest11.countPaths("dac", "out", serviceTest11.getMappaConnessioni(), new HashMap<>());
+		//BigInteger result = serviceTest11.countPaths("svr", "out", serviceTest11.getMappaConnessioni(), new HashMap<>(), "fft", "dac",0);
+        System.out.println("Numero totale di path dac out: " + resultdacout);
+        BigInteger resultfftdac = serviceTest11.countPaths("fft", "dac", serviceTest11.getMappaConnessioni(), new HashMap<>());
+		//BigInteger result = serviceTest11.countPaths("svr", "out", serviceTest11.getMappaConnessioni(), new HashMap<>(), "fft", "dac",0);
+        System.out.println("Numero totale di path fft dac: " + resultfftdac);
+        BigInteger resultdacfft = serviceTest11.countPaths("dac", "fft", serviceTest11.getMappaConnessioni(), new HashMap<>());
+		//BigInteger result = serviceTest11.countPaths("svr", "out", serviceTest11.getMappaConnessioni(), new HashMap<>(), "fft", "dac",0);
+        System.out.println("Numero totale di path dac fft: " + resultdacfft);
+        BigInteger resultsvrfft = serviceTest11.countPaths("svr", "fft", serviceTest11.getMappaConnessioni(), new HashMap<>());
+		//BigInteger result = serviceTest11.countPaths("svr", "out", serviceTest11.getMappaConnessioni(), new HashMap<>(), "fft", "dac",0);
+        System.out.println("Numero totale di path svr fft: " + resultsvrfft);
+        BigInteger resultsvrdac = serviceTest11.countPaths("svr", "dac", serviceTest11.getMappaConnessioni(), new HashMap<>());
+		//BigInteger result = serviceTest11.countPaths("svr", "out", serviceTest11.getMappaConnessioni(), new HashMap<>(), "fft", "dac",0);
+        System.out.println("Numero totale di path svr dac: " + resultsvrdac);
+        BigInteger path_srv_dac_fft_out=BigInteger.ZERO;
+        BigInteger path_srv_fft_dac_out=BigInteger.ZERO;
+        if(resultsvrfft.compareTo(BigInteger.ZERO)>0) {
+        	if(resultfftdac.compareTo(BigInteger.ZERO)>0) {
+        		if(resultdacout.compareTo(BigInteger.ZERO)>0) {
+        			path_srv_fft_dac_out=resultsvrfft.multiply(resultfftdac).multiply(resultdacout);
+        			System.out.println("path_srv_fft_dac_out:"+path_srv_fft_dac_out);
+        		}
+        	}
+        }
+        if(resultsvrdac.compareTo(BigInteger.ZERO)>0) {
+        	if(resultdacfft.compareTo(BigInteger.ZERO)>0) {
+        		if(resultfftout.compareTo(BigInteger.ZERO)>0) {
+        			path_srv_fft_dac_out=resultsvrdac.multiply(resultdacfft).multiply(resultfftout);
+        			System.out.println("path_srv_fft_dac_out:"+path_srv_fft_dac_out);
+        		}
+        	}
+        }
+
+		
 	}
 
 	private static void extracted1(String data, ServiceTest11 serviceTest11, String partenza) {
@@ -33,6 +78,15 @@ public class Test11 {
 		List<List<String>> results=serviceTest11.calcolaPath(partenza);
 		System.out.println("tutti i path sono da "+partenza+" a out sono: "+results.size());
 	}
+	
+	private static void extracted2NV(String data, ServiceTest11 serviceTest11, String partenza) {
+		String[] strParts = data.split("\n");
+		Arrays.stream(strParts).forEach(s -> serviceTest11.populateList(s));
+		partenza="fft";
+		serviceTest11.calcolaPathArrivo("out");
+		List<List<String>> resultsDacToOut=serviceTest11.calcolaPath(partenza);
+		System.out.println("tutti i path sono da "+partenza+" a out sono: "+resultsDacToOut.size());
+		}
 
 	private static void extracted2(String data, ServiceTest11 serviceTest11, String partenza) {
 		String[] strParts = data.split("\n");
@@ -41,43 +95,66 @@ public class Test11 {
 		serviceTest11.calcolaPathArrivo("fft");
 		List<List<String>> resultsfft=serviceTest11.calcolaPath(partenza);
 		System.out.println("tutti i path sono da "+partenza+" a fft sono: "+resultsfft.size());
+		int containDac=0;
+		for(int i=0;i<resultsfft.size();i++) {
+			if(resultsfft.get(i).contains("dac")) {
+				containDac++;
+			}
+		}
+		System.out.println("fft contains dac:"+containDac);
+		if(containDac>0) {
+			//da dac a out
+//			partenza="dac";
+//			serviceTest11.calcolaPathArrivo("out");
+//			List<List<String>> resultsDacToOut=serviceTest11.calcolaPath(partenza);
+//			System.out.println("tutti i path sono da "+partenza+" a out sono: "+resultsDacToOut.size());
+		}
+		System.out.println("containDac:"+containDac);
 		//da srv a dac
+		partenza="svr";
 		serviceTest11.calcolaPathArrivo("dac");
 		List<List<String>> resultsDac=serviceTest11.calcolaPath(partenza);
 		System.out.println("tutti i path sono da "+partenza+" a dac sono: "+resultsDac.size());
-		BigInteger path_srv_dac_fft_out=BigInteger.ZERO;
-		BigInteger path_srv_fft_dac_out=BigInteger.ZERO;
-		if(!resultsfft.isEmpty()) {
-			//da fft a dac
-			partenza="fft";
-			serviceTest11.calcolaPathArrivo("dac");
-			List<List<String>> resultsfftToDac=serviceTest11.calcolaPath(partenza);
-			System.out.println("tutti i path sono da "+partenza+" a dac sono: "+resultsfftToDac.size());
-			if(!resultsfftToDac.isEmpty()) {
-				//da dac a out
-				partenza="dac";
-				serviceTest11.calcolaPathArrivo("out");
-				List<List<String>> resultsDacToOut=serviceTest11.calcolaPath(partenza);
-				System.out.println("tutti i path sono da "+partenza+" a out sono: "+resultsDacToOut.size());
-				path_srv_fft_dac_out=BigInteger.valueOf(resultsfft.size()).multiply(BigInteger.valueOf(resultsfftToDac.size())).multiply(BigInteger.valueOf(resultsDacToOut.size()));
+		int containfft=0;
+		for(int i=0;i<resultsDac.size();i++) {
+			if(resultsfft.get(i).contains("fft")) {
+				containfft++;
 			}
 		}
-		if(!resultsDac.isEmpty()) {
-			//da dac a fft
-			partenza="dac";
-			serviceTest11.calcolaPathArrivo("fft");
-			List<List<String>> resultsDacToFFt=serviceTest11.calcolaPath(partenza);
-			System.out.println("tutti i path sono da "+partenza+" a fft sono: "+resultsDacToFFt.size());
-			if(!resultsDacToFFt.isEmpty()) {
-				//da fft a out
-				partenza="fft";
-				serviceTest11.calcolaPathArrivo("out");
-				List<List<String>> resultsfftToOut=serviceTest11.calcolaPath(partenza);
-				System.out.println("tutti i path sono da "+partenza+" a out sono: "+resultsfftToOut.size());
-				path_srv_dac_fft_out=BigInteger.valueOf(resultsDac.size()).multiply(BigInteger.valueOf(resultsDacToFFt.size())).multiply(BigInteger.valueOf(resultsfftToOut.size()));
-			}
-		}
-		System.out.println("la somma dei path è: "+path_srv_dac_fft_out.add(path_srv_fft_dac_out));
+		System.out.println("containfft:"+containfft);
+//		BigInteger path_srv_dac_fft_out=BigInteger.ZERO;
+//		BigInteger path_srv_fft_dac_out=BigInteger.ZERO;
+//		if(!resultsfft.isEmpty()) {
+//			//da fft a dac
+//			partenza="fft";
+//			serviceTest11.calcolaPathArrivo("dac");
+//			List<List<String>> resultsfftToDac=serviceTest11.calcolaPath(partenza);
+//			System.out.println("tutti i path sono da "+partenza+" a dac sono: "+resultsfftToDac.size());
+//			if(!resultsfftToDac.isEmpty()) {
+//				//da dac a out
+//				partenza="dac";
+//				serviceTest11.calcolaPathArrivo("out");
+//				List<List<String>> resultsDacToOut=serviceTest11.calcolaPath(partenza);
+//				System.out.println("tutti i path sono da "+partenza+" a out sono: "+resultsDacToOut.size());
+//				path_srv_fft_dac_out=BigInteger.valueOf(resultsfft.size()).multiply(BigInteger.valueOf(resultsfftToDac.size())).multiply(BigInteger.valueOf(resultsDacToOut.size()));
+//			}
+//		}
+//		if(!resultsDac.isEmpty()) {
+//			//da dac a fft
+//			partenza="dac";
+//			serviceTest11.calcolaPathArrivo("fft");
+//			List<List<String>> resultsDacToFFt=serviceTest11.calcolaPath(partenza);
+//			System.out.println("tutti i path sono da "+partenza+" a fft sono: "+resultsDacToFFt.size());
+//			if(!resultsDacToFFt.isEmpty()) {
+//				//da fft a out
+//				partenza="fft";
+//				serviceTest11.calcolaPathArrivo("out");
+//				List<List<String>> resultsfftToOut=serviceTest11.calcolaPath(partenza);
+//				System.out.println("tutti i path sono da "+partenza+" a out sono: "+resultsfftToOut.size());
+//				path_srv_dac_fft_out=BigInteger.valueOf(resultsDac.size()).multiply(BigInteger.valueOf(resultsDacToFFt.size())).multiply(BigInteger.valueOf(resultsfftToOut.size()));
+//			}
+//		}
+//		System.out.println("la somma dei path è: "+path_srv_dac_fft_out.add(path_srv_fft_dac_out));
 	}
 	
 	
--- src/main/java/service/ServiceTest11.java
+++ src/main/java/service/ServiceTest11.java
@@ -1,5 +1,6 @@
 package service;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -18,6 +19,7 @@ public class ServiceTest11 {
 
 	private Map<String, List<String>> mappaConnessioni = new HashMap<>();
 	private List<String> listaChiaviArrivo = new ArrayList<>();
+	private List<String> listaChiaviDaEscludere = new ArrayList<>();
 	
 	public void populateList(String s) {
 		String chiave = s.substring(0, s.indexOf(":"));
@@ -34,33 +36,56 @@ public class ServiceTest11 {
 	}
 
 	public List<List<String>> calcolaPath(String partenza) {
-		List<String> valoriPartenza=mappaConnessioni.get(partenza);
+		listaChiaviDaEscludere = new ArrayList<>();
+		List<String> valoriPartenza=new ArrayList<>();
+		valoriPartenza.addAll(mappaConnessioni.get(partenza));
 		List<List<String>> results=new ArrayList();
 		List<String> listaObiettiviRaggiunti=new ArrayList<>();
 		boolean trovato=this.proseguiPath(valoriPartenza, listaObiettiviRaggiunti, results);
-		//System.out.println(results);
+		System.out.println(results);
 		//System.out.println("tutti i path sono: "+results.size());
 		return results;
 		
 	}
+	
+	
+
+	public Map<String, List<String>> getMappaConnessioni() {
+		return mappaConnessioni;
+	}
+
+	public void setMappaConnessioni(Map<String, List<String>> mappaConnessioni) {
+		this.mappaConnessioni = mappaConnessioni;
+	}
 
 	private boolean proseguiPath(List<String> valoriPartenza, List<String> listaObiettiviRaggiunti, List<List<String>> results) {
 		boolean trovato=false;
-		//System.out.println(valoriPartenza);
-		if( valoriPartenza==null) {
+		//System.out.println(valoriPartenza);		
+		
+		if( valoriPartenza==null || valoriPartenza.isEmpty()) {
 			return false;
 		}
+		else {
+			valoriPartenza.removeAll(listaChiaviDaEscludere);
+		}
 		for(String nuovaChiave:valoriPartenza) {
-			if(listaObiettiviRaggiunti.contains(nuovaChiave)){
-				return false;
-			}
-			if(results.contains(listaObiettiviRaggiunti))
-			{
-				return false;
-			}
+//			if(listaObiettiviRaggiunti.contains(nuovaChiave)){
+//				return false;
+//			}
+//			if(results.contains(listaObiettiviRaggiunti))
+//			{
+//				return false;
+//			}
 			if(!listaChiaviArrivo.contains(nuovaChiave)) {
 				listaObiettiviRaggiunti.add(nuovaChiave);
-				trovato=this.proseguiPath(mappaConnessioni.get(nuovaChiave), listaObiettiviRaggiunti,results);
+				valoriPartenza=new ArrayList<>();
+				if(mappaConnessioni.get(nuovaChiave)!=null)
+					valoriPartenza.addAll(mappaConnessioni.get(nuovaChiave));
+				
+				trovato=this.proseguiPath(valoriPartenza, listaObiettiviRaggiunti,results);
+				if(trovato==false) {
+					listaChiaviDaEscludere.add(nuovaChiave);
+				}
 			}
 			else {
 				List<String> listaObiettiviRaggiuntiTemp = new ArrayList<>();
@@ -71,9 +96,9 @@ public class ServiceTest11 {
 			}
 			
 		}
-		if(!listaObiettiviRaggiunti.isEmpty()) {
-			listaObiettiviRaggiunti.remove(listaObiettiviRaggiunti.size()-1);
-		}
+//		if(!listaObiettiviRaggiunti.isEmpty()) {
+//			listaObiettiviRaggiunti.remove(listaObiettiviRaggiunti.size()-1);
+//		}
 		return trovato;
 	}
 
@@ -92,6 +117,102 @@ public class ServiceTest11 {
 			return listaChiavi;
 
 	}
+	
+
+	public  BigInteger countPaths(
+            String current,
+            String target,
+            Map<String, List<String>> graph,
+            Map<String, BigInteger> memo) {
+
+        // Caso base: siamo arrivati a OUT
+        if (current.equals(target)) {
+            return BigInteger.ONE;
+        }
+
+        // Se già calcolato, ritorniamo il valore
+        if (memo.containsKey(current)) {
+            return memo.get(current);
+        }
+
+        BigInteger totalPaths = BigInteger.ZERO;
+
+        // Se il nodo non ha uscite
+        if (!graph.containsKey(current)) {
+            return BigInteger.ZERO;
+        }
+
+        // Visitiamo tutti i nodi successivi
+        for (String next : graph.get(current)) {
+            totalPaths=totalPaths.add(countPaths(next, target, graph, memo));
+        }
+
+        memo.put(current, totalPaths);
+        return totalPaths;
+    }
+
+	
+
+public  BigInteger countPaths(
+            String current,
+            String target,
+            Map<String, List<String>> graph,
+            Map<String, BigInteger> memo, String passaggioIntermedio1, String passaggioIntermedio2, int numeroPassaggioIntermedio) {
+
+        // Caso base: siamo arrivati a OUT
+        if (current.equals(target)) {
+        	if(numeroPassaggioIntermedio==2 || numeroPassaggioIntermedio==4)
+        		return BigInteger.ONE;
+        	else 
+        		return BigInteger.ZERO;
+        }
+        
+        if(numeroPassaggioIntermedio==0 && current.equals(passaggioIntermedio1)) {
+        	numeroPassaggioIntermedio=1;
+        }
+        else if(numeroPassaggioIntermedio==1 && current.equals(passaggioIntermedio2)) {
+        	numeroPassaggioIntermedio=2;
+        }
+        else if(numeroPassaggioIntermedio==0 && current.equals(passaggioIntermedio2)) {
+        	numeroPassaggioIntermedio=3;
+        }
+        else if(numeroPassaggioIntermedio==3 && current.equals(passaggioIntermedio1)) {
+        	numeroPassaggioIntermedio=4;
+        }
+
+        // Se già calcolato, ritorniamo il valore
+        if (memo.containsKey(current) && (numeroPassaggioIntermedio==2 || numeroPassaggioIntermedio==4)) {
+            return memo.get(current);
+        }
+
+        BigInteger totalPaths = BigInteger.ZERO;
+
+        // Se il nodo non ha uscite
+        if (!graph.containsKey(current)) {
+            return BigInteger.ZERO;
+        }
+
+        // Visitiamo tutti i nodi successivi
+        for (String next : graph.get(current)) {
+            totalPaths =totalPaths.add(countPaths(next, target, graph, memo,passaggioIntermedio1, passaggioIntermedio2,numeroPassaggioIntermedio));
+            if(numeroPassaggioIntermedio==2 && next.equals(passaggioIntermedio2)) {
+            	numeroPassaggioIntermedio=1;
+            }
+            else if(numeroPassaggioIntermedio==3 && next.equals(passaggioIntermedio2)) {
+            	numeroPassaggioIntermedio=0;
+            }
+            else if(numeroPassaggioIntermedio==4 && next.equals(passaggioIntermedio1)) {
+            	numeroPassaggioIntermedio=3;
+            }
+            else if(numeroPassaggioIntermedio==1 && next.equals(passaggioIntermedio1)) {
+            	numeroPassaggioIntermedio=0;
+            }
+        }
+        if(totalPaths.compareTo(BigInteger.ZERO)>0)
+        	memo.put(current, totalPaths);
+        return totalPaths;
+    }
+
 	
 
 
